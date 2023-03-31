723f846b5698d919681f81c1b5bcf1f9ba283782
Devestor <crypto.wilkerson@gmail.com>
2023-03-31 22:06:25 +0700

    merge to one file

--- main.go
+++ main.go
@@ -15,12 +15,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/go-co-op/gocron"
+	"github.com/go-co-op/gocron" // ถ้าไม่ต้องการตั้งเวลาให้ลบบรรทัดนี้
 )
 
 func main() {
 	// open file
-	f, err := os.Open("Ex_Ethermail_List.csv") // headers No.,	Ethermail Address,	Token at row 1
+	f, err := os.Open("Ethermail_List.csv") // headers No.,	Ethermail Address,	Token at row 1
+	// f, err := os.Open("Ex_Ethermail_List.csv") // headers No.,	Ethermail Address,	Token at row 1
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -52,11 +53,11 @@ func main() {
 	fmt.Println("Service starting...")
 
 	// Set timezone
-	location, _ := time.LoadLocation("Asia/Bangkok")
+	location, _ := time.LoadLocation("Asia/Bangkok") // ถ้าไม่ต้องการตั้งเวลาให้ลบบรรทัดนี้
 
 	// New scheduler with timezone
-	scheduler := gocron.NewScheduler(location)
-	scheduler.Every(1).Day().At("11:59").Do(func() {
+	scheduler := gocron.NewScheduler(location)       // ถ้าไม่ต้องการตั้งเวลาให้ลบบรรทัดนี้
+	scheduler.Every(1).Day().At("11:59").Do(func() { // ถ้าไม่ต้องการตั้งเวลาให้ลบบรรทัดนี้
 
 		fmt.Println("Starting Send mails")
 		SendMail(mailList, tokenMap)
@@ -65,11 +66,11 @@ func main() {
 		fmt.Println("Starting Read messages")
 		ReadAllMessage(mailList, tokenMap)
 		fmt.Println("End Read messages")
-	})
+	}) // ถ้าไม่ต้องการตั้งเวลาให้ลบบรรทัดนี้
 
 	// start scheduler
-	scheduler.StartAsync()
-	scheduler.StartBlocking()
+	scheduler.StartAsync()    // ถ้าไม่ต้องการตั้งเวลาให้ลบบรรทัดนี้
+	scheduler.StartBlocking() // ถ้าไม่ต้องการตั้งเวลาให้ลบบรรทัดนี้
 }
 
 func ReadAllMessage(mailList []string, tokenMap map[string]string) {
@@ -404,4 +405,122 @@ func GetMailBoxes(token string) (*GetMailBoxesResponse, error) {
 	}
 
 	return xResp, nil
+}
+
+type Payload struct {
+	UploadOnly  bool          `json:"uploadOnly"`
+	IsDraft     bool          `json:"isDraft"`
+	From        From          `json:"from"`
+	To          []To          `json:"to"`
+	Cc          []Cc          `json:"cc"`
+	Bcc         []Bcc         `json:"bcc"`
+	Attachments []Attachments `json:"attachments"`
+	Subject     string        `json:"subject"`
+	Date        string        `json:"date"`
+	Text        string        `json:"text"`
+	HTML        string        `json:"html"`
+}
+type From struct {
+	Address string `json:"address"`
+	Name    string `json:"name"`
+}
+type To struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+type Cc struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+
+type Bcc struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+}
+type Attachments struct {
+	Filename    string `json:"filename"`
+	ContentType string `json:"contentType"`
+	Content     string `json:"content"`
+}
+
+type GetMailBoxesResponse struct {
+	Success bool `json:"success"`
+	Results []struct {
+		ID          string      `json:"id"`
+		Name        string      `json:"name"`
+		Path        string      `json:"path"`
+		SpecialUse  interface{} `json:"specialUse"`
+		ModifyIndex int         `json:"modifyIndex"`
+		Subscribed  bool        `json:"subscribed"`
+		Hidden      bool        `json:"hidden"`
+		Total       int         `json:"total"`
+		Unseen      int         `json:"unseen"`
+		Retention   int64       `json:"retention,omitempty"`
+	} `json:"results"`
+}
+
+type ReponseMakeReadAll struct {
+	Success bool `json:"success"`
+	Updated int  `json:"updated"`
+}
+
+type GetAllMailBoxResponse struct {
+	Success bool `json:"success"`
+	Total   int  `json:"total"`
+	Page    int  `json:"page"`
+	// PreviousCursor bool `json:"previousCursor"`
+	// NextCursor     bool `json:"nextCursor"`
+	Results []struct {
+		ID      int    `json:"id"`
+		Mailbox string `json:"mailbox"`
+		Thread  string `json:"thread"`
+		From    struct {
+			Address string `json:"address"`
+			Name    string `json:"name"`
+		} `json:"from"`
+		To []struct {
+			Address string `json:"address"`
+			Name    string `json:"name"`
+		} `json:"to"`
+		Cc          []interface{} `json:"cc"`
+		Bcc         []interface{} `json:"bcc"`
+		MessageID   string        `json:"messageId"`
+		Subject     string        `json:"subject"`
+		Date        time.Time     `json:"date"`
+		Idate       time.Time     `json:"idate"`
+		Attachments bool          `json:"attachments"`
+		Size        int           `json:"size"`
+		Seen        bool          `json:"seen"`
+		Deleted     bool          `json:"deleted"`
+		Flagged     bool          `json:"flagged"`
+		Draft       bool          `json:"draft"`
+		Answered    bool          `json:"answered"`
+		Forwarded   bool          `json:"forwarded"`
+		References  []interface{} `json:"references"`
+		ContentType struct {
+			Value  string `json:"value"`
+			Params struct {
+				Protocol string `json:"protocol"`
+				Boundary string `json:"boundary"`
+			} `json:"params"`
+		} `json:"contentType"`
+		Encrypted bool `json:"encrypted"`
+	} `json:"results"`
+}
+
+func randomSample(n, k int) []int {
+	if k > n {
+		return nil
+	}
+
+	nums := make([]int, n)
+	for i := range nums {
+		nums[i] = i
+	}
+
+	rand.Shuffle(len(nums), func(i, j int) {
+		nums[i], nums[j] = nums[j], nums[i]
+	})
+
+	return nums[:k]
 }
